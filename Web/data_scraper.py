1f38644656c0a9a3cde838c1c5d15ea34ee61aac
Phillbert Tan <3781117+phillytan@users.noreply.github.com>
2019-07-26 04:37:39 +0800

    Bug Fixes

--- Web/data_scraper.py
+++ Web/data_scraper.py
@@ -3,7 +3,7 @@ import urllib.request
 src_ids = [814173, 814176, 814180, 814241, 810768]
 
 for id in src_ids:
-    url = "http://localhost/amihan/index.php?action=batch_update&src_id=" + str(id)
+    url = "http://localhost/update?src_id=" + str(id)
     res = urllib.request.urlopen(url).read()
     print("Connecting to: ", url)
     print(res)
