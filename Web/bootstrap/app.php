9c39c420cc115b05b258b5971d608f6844a0b420
Phillbert Tan <mrsilver512@users.noreply.github.com>
2019-07-31 17:56:33 +0800

    Bug Fixes

--- Web/bootstrap/app.php
+++ Web/bootstrap/app.php
@@ -31,4 +31,17 @@ $container['errorHandler'] = function ($container) {
 };
 
 $app = new \Slim\App($container);
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 require_once "routes.php";
