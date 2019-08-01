4bf2b58fa757180200fe1479bcada4035051a802
Adrian Serapio <adtserapio@gmail.com>
2019-08-01 21:30:28 +0800

    Fixed FutureBuilder to integrate new APIfor Air page

--- Mobile/lib/data/air.dart
+++ Mobile/lib/data/air.dart
@@ -15,11 +15,8 @@ import 'package:env_ph/constants.dart';
 
 import 'package:env_ph/data/locations.dart';
 
-
 DataFeed dataFeed;
-Locations sensorLocations;
-int location_id = 810768;
-
+int location_id = 814176;
 
 var pageOptions = [AirPage(), HistoryPage(), Text("HE")];
 
@@ -68,54 +65,64 @@ class AirPage extends StatefulWidget {
   AirPageState createState() => AirPageState();
 }
 
-Future<DataFeed> getJsonData(int id) async {
+Future<Locations> getLocationJsonData() async {
 
-  print("HELLO FROM JSON");
-
-
-  var response = await http.get(url + id.toString());
+  Locations sensorLocations;
 
   var sensorResponse = await http.get(urlLocations);
+
+  if (sensorResponse.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON
+    var locationsData = json.decode(sensorResponse.body);
+
+     sensorLocations = new Locations.fromJson(locationsData);
+
+
+  } else {
+    // If that call was not successful, throw an error.
+  }
+
+  return sensorLocations;
+}
+
+Future<DataFeed> getSensorJsonData(int id) async {
+
+  var response = await http.get(url + id.toString());
 
   if (response.statusCode == 200) {
     // If the call to the server was successful, parse the JSON
     var data = json.decode(response.body);
-    var locationsData = json.decode(sensorResponse.body);
 
     dataFeed = new DataFeed.fromJson(data);
-    sensorLocations = new Locations.fromJson(locationsData);
 
   } else {
     // If that call was not successful, throw an error.
   }
 
   return dataFeed;
-
-
 }
 
 class AirPageState extends State<AirPage> {
   bool loaded = false;
   DataFeed dataFeed;
-  Locations sensorLocations;
-  int location_id = 810768;
 
   void initState() {
 
+    td.text = "Bonuan Gueset, Dagupan, 2400 Pangasinan";
+
     loaded = true;
     super.initState();
-
-    getJsonData(location_id);
 
 
     location.onLocationChanged().listen((value) {
       if (loaded) {
         setState(() {
           userLocation = value;
+
         });
       }
-    });
 
+    });
   }
 
   void dispose() {
@@ -129,22 +136,18 @@ class AirPageState extends State<AirPage> {
     });
   }
 
-  List<Widget> generateDataTiles() {
+  List<Widget> generateDataTiles(DataFeed feed) {
 
-    List<DataTile> data = List(0);
 
-    if(dataFeed != null) {
       List<DataTile> data = List(dataTypes.length);
 
-      data[0] = DataTile(0, dataFeed.latest[0].temp.toString());
-      data[1] = DataTile(1, dataFeed.latest[0].humidity.toString());
-      data[2] = DataTile(2, dataFeed.latest[0].carbonMonoxide.toString());
-      data[3] = DataTile(3, dataFeed.latest[0].carbonMonoxide.toString());
-      data[4] = DataTile(4, dataFeed.latest[0].pM_1.toString());
-      data[5] = DataTile(5, dataFeed.latest[0].pM_1.toString());
-      data[6] = DataTile(6, dataFeed.latest[0].pM_2_5.toString());
-    }
-
+      data[0] = DataTile(0, feed.latest[0].temp.toString());
+      data[1] = DataTile(1, feed.latest[0].humidity.toString());
+      data[2] = DataTile(2, feed.latest[0].carbonMonoxide.toString());
+      data[3] = DataTile(3, feed.latest[0].carbonMonoxide.toString());
+      data[4] = DataTile(4, feed.latest[0].pM_1.toString());
+      data[5] = DataTile(5, feed.latest[0].pM_1.toString());
+      data[6] = DataTile(6, feed.latest[0].pM_2_5.toString());
 
 
     return data;
@@ -157,9 +160,11 @@ class AirPageState extends State<AirPage> {
     });
   }
 
+  List<String> locations;
+
+
   bool typing = false;
   TextEditingController td = TextEditingController();
-
 
   List<String> items = [];
 
@@ -167,188 +172,189 @@ class AirPageState extends State<AirPage> {
   @override
   Widget build(BuildContext context) {
 
-    print("HELLO FROM BUILD");
 
-    List<String> locations = List(0);
-
-
-    if (sensorLocations != null) {
-      List<String> locations = List(sensorLocations.sensors.length);
-      for (var i = 0; i < sensorLocations.sensors.length; i ++) {
-        locations[i] = sensorLocations.sensors[i].location_name;
-      }
-    }
-
-    final width = MediaQuery
-        .of(context)
-        .size
-        .width;
-    final height = MediaQuery
-        .of(context)
-        .size
-        .height;
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        body: FutureBuilder(
-            future: getJsonData(location_id),
-            builder: (context, snapshot) {
+        body: Stack(
+      children: <Widget>[
+        Positioned(
+            top: 10,
+            right: 10,
+            child: Hero(
+                tag: "toggleLang",
+                child: FittedBox(
+                    child: RawMaterialButton(
+                        onPressed: toggleLang,
+                        child: Text(langs[lang_idx],
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Avenir',
+                                fontSize: 15)),
+                        shape: CircleBorder(),
+                        fillColor: colorBtn,
+                        splashColor: colorBtnSelected,
+                        elevation: 2,
+                        padding: EdgeInsets.all(10))))),
+        Positioned(
+          top: 95,
+          left: 25,
+          child: Container(
+            height: 80,
+            width: width,
+            child: Text("AIR QUALITY", style: TextStyle(fontFamily: "Avenir", fontWeight: FontWeight.w900, fontSize: 40, color: colorText)),
+          )
+        ),
 
-              switch(snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  print(snapshot.connectionState);
-                  return Center(child: CircularProgressIndicator());
-                default:
-                  return Stack(
-                    children: <Widget>[
-                      Positioned(
-                          top: 10,
-                          right: 10,
-                          child: Hero(
-                              tag: "toggleLang",
-                              child: FittedBox(
-                                  child: RawMaterialButton(
-                                      onPressed: toggleLang,
-                                      child: Text(langs[lang_idx],
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontWeight: FontWeight.bold,
-                                              fontFamily: 'Avenir',
-                                              fontSize: 15)),
-                                      shape: CircleBorder(),
-                                      fillColor: colorBtn,
-                                      splashColor: colorBtnSelected,
-                                      elevation: 2,
-                                      padding: EdgeInsets.all(10))))),
+        Positioned(
+            top: height / 5,
+            left: 25,
+            child: Container(
+                height: height / 4,
+                width: width - 50,
+                child: FutureBuilder(
+                    future: getLocationJsonData(),
+                    builder: (context, snapshot) {
 
+                      if (snapshot.data != null) {
 
-                      Positioned(
-                          top: height / 5,
-                          left: 25,
-                          child: Container(
-                            width: width - 50,
-                            child: TextField(
+                        locations = List(snapshot.data.sensors.length);
+                        for (var i = 0; i < snapshot.data.sensors.length; i++) {
+                          locations[i] = snapshot.data.sensors[i].location_name;
 
-                              controller: td,
-                              decoration: InputDecoration(
-                                  focusedBorder: OutlineInputBorder(
-                                    borderSide: new BorderSide(
-                                        color: colorBtn, width: 2),
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  enabledBorder: OutlineInputBorder(
-                                    borderSide: new BorderSide(
-                                        color: colorBtn, width: 2),
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  prefixIcon: Icon(
-                                      Icons.my_location, color: colorBtn)
+                          utilSensorLocations[snapshot.data.sensors[i].location_name] = Coordinate(snapshot.data.sensors[i].longitude, snapshot.data.sensors[i].latitude);
+
+                        }
+
+                        return Stack(children: [TextField(
+                          onTap: () {
+                            td.clear();
+                            setState((){
+                              typing = true;
+                            });
+                          },
+                          controller: td,
+                          decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide:
+                                    new BorderSide(color: colorBtn, width: 2),
+                                borderRadius: BorderRadius.circular(20),
                               ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    new BorderSide(color: colorBtn, width: 2),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              prefixIcon:
+                                  Icon(Icons.my_location, color: colorBtn)),
+                          onChanged: (text) {
 
-                              onTap: () {
-                                setState(() {
-                                  typing = true;
-                                });
-                              },
-                              onChanged: (text) {
+                            List<String> dummySearchList = List<String>();
 
-                                List<String> dummySearchList = List<String>();
-                                dummySearchList.addAll(locations);
 
-                                List<String> dummyListData = List<String>();
-                                dummySearchList.forEach((item) {
-                                  if (item.contains(text)) {
-                                    dummyListData.add(item);
-                                  }
-                                });
+                            dummySearchList.addAll(locations);
 
-                                setState(() {
+                            List<String> dummyListData = List<String>();
+
+                            dummySearchList.forEach((item) {
+
+                              if (item.contains(text)) {
+                                dummyListData.add(item);
+                              }
+                            });
+
+                            setState(
+                                () {
                                   items.clear();
                                   items.addAll(dummyListData);
-                                });
-                                return;
-                              },
-                              onEditingComplete: () {
-                                setState(() {
-                                  typing = false;
-                                  FocusScope.of(context).requestFocus(
-                                      new FocusNode());
-                                });
-                                setState(() {
-                                  items.clear();
-                                  items.addAll(locations);
-                                });
-                              },
-                            ),
-                          )
-                      ),
-
-                      typing ? Positioned(
-                        top: 225,
-                        left: 25,
-                        child: Container(
-                            width: width - 50,
-                            height: 300,
-                            child: ListView.builder(
-                                itemCount: items.length,
-                                itemBuilder: (context, index) {
-                                  return ListTile(
-                                    onTap: () {
-                                      print(index);
-                                      td.text = items[index];
-                                      print(sensorLocations.sensors[index].src_id);
-                                      setState(
-                                              () {
-                                            typing = false;
-                                            location_id = sensorLocations.sensors[index].src_id;
-                                          }
-                                      );
-                                      FocusScope.of(context).requestFocus(
-                                          new FocusNode());
-                                    },
-                                    leading: Icon(Icons.location_on),
-                                    title: Text('${items[index]}'),
-                                  );
-                                })
+                                }
+                            );
+                            return;
+                          },
+                          onEditingComplete: () {
+                            setState(() {
+                              typing = false;
+                              FocusScope.of(context)
+                                  .requestFocus(new FocusNode());
+                            });
+                            setState(() {
+                              items.clear();
+                              items.addAll(locations);
+                            });
+                          },
                         ),
-                      ) : Container(),
+
+                          typing ? Positioned(
+                            top:  65,
+                            left: 25,
+                            child: Container(
+                                width: width - 50,
+                                height: 250,
+                                child: ListView.builder(
+                                    itemCount: items.length,
+                                    itemBuilder: (context, index) {
+                                      return ListTile(
+                                        onTap: () {
+                                          td.text = items[index];
+                                          location_id =
+                                          snapshot.data.sensors[index].src_id;
+
+                                          setState(() {
+                                            typing = false;
+                                          });
 
 
-                      !typing ? Container(
-                          height: MediaQuery
-                              .of(context)
-                              .size
-                              .height / 2,
-                          margin: EdgeInsets.fromLTRB(
-                              20,
-                              MediaQuery
-                                  .of(context)
-                                  .size
-                                  .height / 2.75,
-                              0,
-                              0),
-                          child: GridView.count(
-                            // Create a grid with 2 columns. If you change the scrollDirection to
-                            // horizontal, this would produce 2 rows.
-                              crossAxisCount: 1,
-                              scrollDirection: Axis.horizontal,
-                              mainAxisSpacing: 2,
-                              childAspectRatio: (width) /
-                                  (MediaQuery
-                                      .of(context)
-                                      .size
-                                      .height / 3),
-                              children: generateDataTiles())) : Container(),
-                    ],
-                  );
+                                          FocusScope.of(context)
+                                              .requestFocus(new FocusNode());
+                                        },
+                                        leading: Icon(Icons.location_on),
+                                        title: Text('${items[index]}'),
+                                      );
+                                    })),
+                          ) : Container(),
 
 
-              }
+                        ]);
+                      } else {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                    }))),
+        !typing ? Positioned(
+          top: -20,
+          left: 0,
+          child:
+        Container(
+          width: width,
+            height: 750,
+            child: FutureBuilder(
+                future: getSensorJsonData(location_id),
+                builder: (context, snapshot) {
+                  if (snapshot.data != null) {
 
-            }));
+                    return Container(
+                        height: MediaQuery.of(context).size.height / 2,
+                        margin: EdgeInsets.fromLTRB(
+                            20, MediaQuery.of(context).size.height / 2.75, 0, 0),
+                        child: GridView.count(
+                          // Create a grid with 2 columns. If you change the scrollDirection to
+                          // horizontal, this would produce 2 rows.
+                            crossAxisCount: 1,
+                            scrollDirection: Axis.horizontal,
+                            mainAxisSpacing: 2,
+                            childAspectRatio:
+                            (width) / (MediaQuery.of(context).size.height / 3),
+                            children: generateDataTiles(snapshot.data)));
+
+                  } else {
+                    return Center(child:CircularProgressIndicator());
+                  }
+                }
+            )
+        )) : Container()
+
+      ],
+    ));
   }
-
-
 }
-
-
-
