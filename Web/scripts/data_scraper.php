0c114a5ee1569453392ebd13c8520ac94bbf1796
Phillbert Tan <3781117+phillytan@users.noreply.github.com>
2019-08-04 18:23:35 +0800

    Bug Fixes

--- Web/scripts/data_scraper.php
+++ Web/scripts/data_scraper.php
@@ -28,14 +28,14 @@ foreach ($ids as &$src_id) {
         $last_update = formatDate($channel->updated_at);
         $last_entry_id = $channel->last_entry_id;
         $feed = $jobj->feeds;
-        $getLastEntryIdStmt = $mysqli->prepare("SELECT last_entry_id FROM sensor_map WHERE src_id=?");
+        $getLastEntryIdStmt = $mysqli->prepare("SELECT entry_id FROM sensor_data WHERE src_id=? ORDER BY entry_time DESC LIMIT 1;");
         if (!$getLastEntryIdStmt) echo "$src_id: ERROR 12201: $err";
         $getLastEntryIdStmt->bind_param("i", $src_id);
         $res = $getLastEntryIdStmt->execute();
         if (!$res) echo "$src_id: ERROR 12202: 'Error querying database. $getLastEntryIdStmt->error";
         else {
             $getLastEntryIdRow = $getLastEntryIdStmt->get_result()->fetch_assoc();
-            if ($getLastEntryIdRow != null) $cache_id = $getLastEntryIdRow['last_entry_id'];
+            if ($getLastEntryIdRow != null) $cache_id = $getLastEntryIdRow['entry_id'];
             else $cache_id = -1;
         }
         $getLastEntryIdStmt->close();
@@ -43,14 +43,12 @@ foreach ($ids as &$src_id) {
             // New sensor. Register
             // Action Code: 1 
             $sql = "INSERT INTO sensor_map
-            (src_id, location_name, latitude, longitude, creation_time, last_update, last_entry_id)
-            VALUES (?, ?, ?, ?, ?, ?, ?)";
+            (src_id, location_name, latitude, longitude, creation_time, last_update)
+            VALUES (?, ?, ?, ?, ?, ?)";
             $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("isddssi", $src_id, $location_name, $latitude, $longitude, $creation_date, $last_update, $last_entry_id);
+            $stmt->bind_param("isddss", $src_id, $location_name, $latitude, $longitude, $creation_date, $last_update);
             $res = $stmt->execute();
-            if (!$res) {
-                echo "$src_id: ERROR 11011: Error registering new sensor. $stmt->error";
-            }
+            if (!$res) echo "$src_id: ERROR 11011: Error registering new sensor. $stmt->error";
             $stmt->close();
         }
         if ($cache_id < $last_entry_id) {
@@ -61,21 +59,8 @@ foreach ($ids as &$src_id) {
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             $stmt_data = $mysqli->prepare($sql_data);
 
-            $sql_update = "UPDATE sensor_map
-                SET last_update = CASE
-                    WHEN last_update < ? THEN ?
-                    ELSE last_update
-                END,
-                last_entry_id = CASE
-                    WHEN last_entry_id < ? THEN ?
-                    ELSE last_entry_id
-                END
-                WHERE src_id=?";
-            $stmt_update = $mysqli->prepare($sql_update);
-
-            if ($stmt_data === FALSE || $stmt_update === FALSE) {
-                echo "$src_id: ERROR 11002: $mysqli->error";
-            } else {
+            if (!$stmt_data) echo "$src_id: ERROR 11002: $mysqli->error";
+            else {
                 $count = 0;
                 foreach ($feed as $entry) {
                     $entry_id = $entry->entry_id;
@@ -93,22 +78,14 @@ foreach ($ids as &$src_id) {
                         $stmt_data->bind_param("iisddddddd", $src_id, $entry_id, $entry_time,
                             $pm1, $pm2_5, $pm10, $humidity, $temperature, $voc, $carbon_monoxide);
                         $res = $stmt_data->execute();
-                        if (!$res) {
-                            echo "$src_id: ERROR 11003: $stmt_data->error";
-                        }
-                        $stmt_update->bind_param("ssiii", $entry_time, $entry_time, $entry_id, $entry_id, $src_id);
-                        $res = $stmt_update->execute();   
-                        if (!$res) {
-                            echo "$src_id: ERROR 11004: $stmt_update->error";
-                        }
+                        if (!$res) echo "$src_id: ERROR 11003: $stmt_data->error";
                     }
                 }
                 $stmt_data->close();
-                $stmt_update->close();
-                echo "$src_id: $count entries inserted for sensor.<br>";
+                echo "$src_id: $count entries inserted for sensor.";
             }
         } else {
-            echo "$src_id: No new data.<br>";
+            echo "$src_id: No new data.";
         }
     }
     echo "\n";
