4505c66dcaa81fb76520624978492ed117842349
Phillbert Tan <mrsilver512@users.noreply.github.com>
2019-08-06 01:22:58 +0800

    Added Google Analytics

--- React/src/index.js
+++ React/src/index.js
@@ -3,6 +3,10 @@ import { render } from 'react-snapshot';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-145147538-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 render(<App />, document.getElementById('root'));
 
