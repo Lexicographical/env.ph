f3f98e8d75140c8541a9ed27db095e2e2aff86c0
Phillbert Tan <3781117+phillytan@users.noreply.github.com>
2019-08-13 11:18:20 +0800

    Added API Auth using JWT

---
+++ Web/app/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers;
+
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+
+class AuthController extends BaseController {
+    public function register($req, $response) {
+        $user = $req->getParsedBody();
+        if (!(isset($user['name']) && isset($user['email']) && isset($user['password']))) return $response->withStatus(400)->withJson(["error" => true, "message" => "Missing parameters."]);
+        $e = $user['email'];
+        $stmt = $this->mysqli->prepare("SELECT COUNT(*) FROM users WHERE email=?");
+        $stmt->bind_param("s", $e);
+        $res = $stmt->execute();
+        $row = $stmt->get_result()->fetch_array();
+        if ($row[0] > 0) return $response->withStatus(400)->withJson(["error" => true, "message" => "User $e already exists."]);
+        $stmt = $this->mysqli->prepare("INSERT INTO users (name, email, password, type) VALUES (?, ?,  ?, 'user');");
+        $p = password_hash($user['password'], PASSWORD_BCRYPT);
+        $stmt->bind_param("sss", $user['name'], $e, $p);
+        $res = $stmt->execute();
+        $time = time();
+        $signer = new Sha256();
+        if (isset($_ENV['SECRET_KEY'])) $key = $_ENV['SECRET_KEY'];
+        else $key = 'Project Amihan --- this is a secret please change this.';
+        $token = (new Builder())->issuedBy('https://api.amihan.xyz')->permittedFor('https://amihan.xyz')->identifiedBy($e, true)->issuedAt($time)->canOnlyBeUsedAfter($time + 60)->expiresAt($time + 3600)->getToken($signer, new Key($key));
+        return $response->write($token);
+    }
+    public function authenticate($req, $response) {
+        $user = $req->getParsedBody();
+        if (!(isset($user['email']) && isset($user['password']))) return $response->withStatus(400)->withJson(["error" => true, "message" => "Missing parameters."]);
+        $e = $user['email'];
+        $p = $user['password'];
+        $stmt = $this->mysqli->prepare("SELECT * FROM users WHERE email=?");
+        $stmt->bind_param("s", $e);
+        $res = $stmt->execute();
+        $row = $stmt->get_result()->fetch_array();
+        if (!isset($row[0])) return $response->withStatus(401)->withJson(["error" => true, "message" => "User not found"]);
+        if (!password_verify($p, $row['password'])) return $response->withStatus(401)->withJson(["error" => true, "message" => "Incorrect Password"]);
+        $time = time();
+        $signer = new Sha256();
+        if (isset($_ENV['SECRET_KEY'])) $key = $_ENV['SECRET_KEY'];
+        else $key = 'Project Amihan --- this is a secret please change this.';
+        $token = (new Builder())->issuedBy('https://api.amihan.xyz')->permittedFor('https://amihan.xyz')->identifiedBy($e, true)->issuedAt($time)->canOnlyBeUsedAfter($time + 60)->expiresAt($time + 3600)->getToken($signer, new Key($key));
+        return $response->write($token);
+    }
+}
---
+++ Web/app/Middleware/JWTMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Middleware;
+
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\ValidationData;
+
+class JWTMiddleware extends BaseMiddleware
+{
+    public function __invoke($request, $response, $next) {
+        $response = $next($request, $response);
+        if (!isset($request->getHeaders()['HTTP_AUTHORIZATION'])) return $response->withStatus(401)->withJson([ 'error' => true, 'message' => "JWT Token Required" ]);
+        $token = explode(" ", $request->getHeaders()['HTTP_AUTHORIZATION'][0])[1];
+        $token = (new Parser())->parse((string) $token);
+        $data = new ValidationData();
+        $data->setIssuer('https://api.amihan.xyz');
+        $data->setAudience('https://amihan.xyz');
+        if (!$token->validate($data)) return $response->withStatus(401)->withJson([ 'error' => true, 'message' => "JWT Token Not Valid" ]); 
+        $request = $request->withAttribute('user', $token->getHeader('jti'));
+        return $next($request, $response);
+    }
+}
