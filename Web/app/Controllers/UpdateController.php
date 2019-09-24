51e9d1fedacd75f6c9ad3ed1415d2d83bc36cd4c
GREGORY WILLIAM JOSEPH D. LIU <gram.liu226@gmail.com>
2019-09-24 18:45:26 -0400

    Integrated UpdateController with api verification

--- Web/app/Controllers/UpdateController.php
+++ Web/app/Controllers/UpdateController.php
@@ -3,9 +3,9 @@
 namespace App\Controllers;
 
 class UpdateController extends BaseController {
-    private function verifyApiKey($mysql, $src_id, $api_key) {
+    private function verifyApiKey($src_id, $api_key) {
         $sql = "SELECT api_key FROM sensor_map WHERE src_id=?";
-        $stmt = $mysql->prepare($sql);
+        $stmt = $this->mysqli->prepare($sql);
         $stmt->bind_param("i", $src_id);
         $res = $stmt->execute();
 
@@ -24,43 +24,69 @@ class UpdateController extends BaseController {
         }
     }
 
+    private function getLastEntryId($src_id) {
+        $sql = "SELECT MAX(entry_id) FROM sensor_data WHERE src_id=?";
+        $stmt = $this->mysqli->prepare($sql);
+        $stmt->bind_param("i", $src_id);
+        $res = $stmt->execute();
+
+        if (!$res) {
+            error("Error querying database to get last entry id", 
+                array("src" => "UpdateController::getLastEntryId", "src_id" => $src_id));
+            return false;
+        } else {
+            $row = $stmt->get_result()->fetch_assoc();
+            if ($row == null) {
+                error("Error no sensor found with src_id " . $src_id, 
+                    array("src" => "UpdateController::getLastEntryId", "src_id" => $src_id));
+                return false;
+            } else {
+                $entry_id = $row["MAX(entry_id)"];
+                if ($entry_id === NULL) {
+                    $entry_id = 0;
+                }
+                return $entry_id + 1;
+            }
+        }
+    }
+
     public function update($req, $response) {
         if (!isset($req->getQueryParams()["src_id"],
             $req->getQueryParams()["api_key"],
-            $req->getQueryParams()["field1"],
-            $req->getQueryParams()["field2"],
-            $req->getQueryParams()["field3"],
-            $req->getQueryParams()["field4"],
-            $req->getQueryParams()["field5"],
-            $req->getQueryParams()["field6"],
-            $req->getQueryParams()["field7"]
+            $req->getQueryParams()["pm1"],
+            $req->getQueryParams()["pm2_5"],
+            $req->getQueryParams()["pm10"],
+            $req->getQueryParams()["humidity"],
+            $req->getQueryParams()["temperature"],
+            $req->getQueryParams()["voc"],
+            $req->getQueryParams()["carbon_monoxide"]
         )) {
             return $response->withStatus(400)->withJson(['error' => true, 'message' => 'Missing parameters']);
         } else {
             $src_id = $req->getQueryParams()["src_id"];
             $api_key = $req->getQueryParams()["api_key"];
 
-            if (!verifyApiKey($this->mysqli, $src_id, $api_key)) {
+            if (!$this->verifyApiKey($src_id, $api_key)) {
                 return $response->withStatus(400)->withJson(['error' => true, 'message' => 'API Key is invalid!']);
             }
 
             $sql_data = "INSERT INTO sensor_data
             (src_id, entry_id, entry_time, pm1, pm2_5, pm10, humidity, temperature, voc, carbon_monoxide)
             VALUES (?, ?, NOW(), ?, ?, ?, ?, ?, ?, ?)";
-            $stmt_data = $this->$mysqli->prepare($sql_data);
+            $stmt_data = $this->mysqli->prepare($sql_data);
 
             if (!$stmt_data) {
-                error("Error occured: ".$mysqli->error, array("src" => "UpdateController::update", "breakpoint" => "1"));
+                error("Error occured: ".$this->mysqli->error, array("src" => "UpdateController::update", "breakpoint" => "1"));
                 return $response->withStatus(400)->withJson(['error' => true, 'message' => 'Error occured.']);
             } else {
-                $entry_id = getLastEntryId();
-                $pm1 = $req->getQueryParams()["field1"];
-                $pm2_5 = $req->getQueryParams()["field2"];
-                $pm10 = $req->getQueryParams()["field3"];
-                $humidity = $req->getQueryParams()["field4"];
-                $temperature = $req->getQueryParams()["field5"];
-                $voc = $req->getQueryParams()["field6"];
-                $carbon_monoxide = $req->getQueryParams()["field7"];
+                $entry_id = $this->getLastEntryId($src_id);
+                $pm1 = $req->getQueryParams()["pm1"];
+                $pm2_5 = $req->getQueryParams()["pm2_5"];
+                $pm10 = $req->getQueryParams()["pm10"];
+                $humidity = $req->getQueryParams()["humidity"];
+                $temperature = $req->getQueryParams()["temperature"];
+                $voc = $req->getQueryParams()["voc"];
+                $carbon_monoxide = $req->getQueryParams()["carbon_monoxide"];
 
                 $stmt_data->bind_param("iiddddddd", $src_id, $entry_id,
                     $pm1, $pm2_5, $pm10, $humidity, $temperature, $voc, $carbon_monoxide);
@@ -70,7 +96,7 @@ class UpdateController extends BaseController {
                     return $response->withStatus(400)->withJson(['error' => true, 'message' => 'Error executing update.']);
                 }
                 $stmt_data->close();
-                info("Inserted new data", array("src_id" => $src_id, "count" => $count));
+                return $response->withStatus(200);
             }
         }
     }
