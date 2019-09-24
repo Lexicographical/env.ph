a92b204521585558587b2bf0fa8eb4c1caa9f219
GREGORY WILLIAM JOSEPH D. LIU <gram.liu226@gmail.com>
2019-09-24 18:45:26 -0400

    Integrated UpdateController with api verification

--- Mobile/lib/home.dart
+++ Mobile/lib/home.dart
@@ -22,53 +22,57 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: new Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Positioned(
-            top: 10,
-            right: 10,
-            child: Hero(
-                tag: "toggleLang",
-                child: FittedBox(
-                    child: RawMaterialButton(
-                        onPressed: toggleLang,
-                        child: Text(langs[lang_idx],
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Avenir',
-                                fontSize: 15)),
-                        shape: CircleBorder(),
-                        fillColor: colorBtn,
-                        splashColor: colorBtnSelected,
-                        elevation: 2,
-                        padding: EdgeInsets.all(10))))),
-        Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          fit: StackFit.expand,
           children: <Widget>[
-            IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(SlideRightRoute(widget: AirControl()));
-                },
-                iconSize: 150,
-                icon: Image(
-                    image: AssetImage("assets/images/env_air_button.png"))),
-            Text("AIR QUALITY", style: styleHomeText),
-            Padding(
-              padding: EdgeInsets.all(30),
-            ),
-            IconButton(
-                onPressed: () {
-                  Navigator.of(context).push(SlideRightRoute(widget: WaterPage()));
-                },
-                iconSize: 150,
-                icon: Image(
-                    image: AssetImage("assets/images/env_water_button.png"))),
-            Text("WATER QUALITY", style: styleHomeText),
+            Positioned(
+                top: 10,
+                right: 10,
+                child: Hero(
+                    tag: "toggleLang",
+                    child: FittedBox(
+                        child: RawMaterialButton(
+                            onPressed: toggleLang,
+                            child: Text(langs[lang_idx],
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Avenir',
+                                    fontSize: 15)),
+                            shape: CircleBorder(),
+                            fillColor: colorBtn,
+                            splashColor: colorBtnSelected,
+                            elevation: 2,
+                            padding: EdgeInsets.all(10))))),
+            Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                              SlideRightRoute(widget: AirControl()));
+                        },
+                        iconSize: 150,
+                        icon: Image(
+                            image: AssetImage(
+                                "assets/images/env_air_button.png"))),
+                    Text("AIR QUALITY", style: styleHomeText),
+                    Padding(
+                      padding: EdgeInsets.all(30),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                              SlideRightRoute(widget: WaterPage()));
+                        },
+                        iconSize: 150,
+                        icon: Image(
+                            image: AssetImage(
+                                "assets/images/env_water_button.png"))),
+                    Text("WATER QUALITY", style: styleHomeText),
+                  ],
+                )),
           ],
-        )),
-      ],
-    ));
+        ));
   }
 }
